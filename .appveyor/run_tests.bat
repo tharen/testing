7a719f849789c2fe53be0d0370e4fbbdb3e7ec1a
Tod Haren <tod.haren@gmail.com>
2016-01-15 00:51:05 -0800

    Reconfigure appveyor build

--- .appveyor/run_tests.bat
+++ .appveyor/run_tests.bat
@@ -3,9 +3,9 @@
 
 set PYTHONPATH=%APPVEYOR_BUILD_FOLDER%\bin\build\Open-FVS\python;%PYTHONPATH%
 
-call %PYTHON%\python.exe -c "import numpy;print('Numpy version:',numpy.version.version)"
+call %PYTHON_HOME%\python.exe -c "import numpy;print('Numpy version:',numpy.version.version)"
 
 pushd %APPVEYOR_BUILD_FOLDER%\bin\build\Open-FVS\python\test
-call %PYTHON%\python.exe -m unittest test_variants
+call %PYTHON_HOME%\python.exe -m unittest test_variants
 
 popd
