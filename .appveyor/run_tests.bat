aa59b0bebeaac0e9ef6a4a8d6493296459eaa938
Tod Haren <tod.haren@gmail.com>
2016-01-14 17:50:15 -0800

    Switch to cPython instead of miniconda.

--- .appveyor/run_tests.bat
+++ .appveyor/run_tests.bat
@@ -3,6 +3,8 @@
 
 set PYTHONPATH=%APPVEYOR_BUILD_FOLDER%\bin\build\Open-FVS\python;%PYTHONPATH%
 
+call %PYTHON%\python.exe -c "import numpy;print('Numpy version:',numpy.version.version)"
+
 pushd %APPVEYOR_BUILD_FOLDER%\bin\build\Open-FVS\python\test
 call %PYTHON%\python.exe -m unittest test_variants
 
