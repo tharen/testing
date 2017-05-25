976d5fa5efed4161c89699d11817671bc529f375
Tod Haren <tod.haren@gmail.com>
2017-05-25 14:25:49 -0700

    [skip appveyor]

--- .travis-ci/install_miniconda.sh
+++ .travis-ci/install_miniconda.sh
@@ -21,7 +21,7 @@ else
     
     chmod +x $HOME/download/miniconda.sh
     $HOME/download/miniconda.sh -b -p $HOME/miniconda
-    conda update -q conda
+    $HOME/miniconda/bin/conda update -q conda
     
 fi
 
